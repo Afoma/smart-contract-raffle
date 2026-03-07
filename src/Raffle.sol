8ae2c6eeeadc035cd797d5c3c6a71baea1f4705c
Afoma <antoinetteorji@gmail.com>
2026-03-07 01:25:57 +0100

    Initial commit

---
+++ src/Raffle.sol
@@ -0,0 +1,106 @@
+// Layout of the contract file:
+// version
+// imports
+// errors
+// interfaces, libraries, contract
+
+// Inside Contract:
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
+/**
+ * @title A sample Raffle Contract
+ * @author Afoma Orji
+ * @notice This contract is for creating a sample raffle
+ * @dev It implements Chainlink VRFv2.5 and Chainlink Automation
+ */
+
+contract Raffle is VRFConsumerBaseV2Plus{
+
+    /* Errors */
+    error Raffle__NotEnoughEthSent();
+
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+    uint256 private immutable i_entranceFee;
+    // @dev The duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    uint32 private immutable i_callbackGasLimit;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+
+    /** Events **/
+
+    event RaffleEntered(address indexed player);
+
+    constructor(
+        uint256 entranceFee, 
+        uint256 interval, 
+        address vrfCoordinator, 
+        bytes32 gasLane, 
+        uint256 subscriptionId, 
+        uint32 callbackGasLimit
+    )   VRFConsumerBaseV2Plus(vrfCoordinator){
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+    }
+
+    function enterRaffle() public payable{
+         if(msg.value < i_entranceFee) revert Raffle__NotEnoughEthSent();
+         s_players.push(payable(msg.sender));
+         emit RaffleEntered(msg.sender);
+    }
+
+    function pickWinner() public {
+        if ((block.timestamp - s_lastTimeStamp < i_interval)){
+            revert();
+        }
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_keyHash,
+            subId: i_subscriptionId,
+            requestConfirmations: REQUEST_CONFIRMATIONS,
+            callbackGasLimit: i_callbackGasLimit,
+            numWords: NUM_WORDS,
+            extraArgs: VRFV2PlusClient._argsToBytes(
+                VRFV2PlusClient.ExtraArgsV1({nativePayment: false}))
+        });
+
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
+    } 
+
+    function fulfillRandomWords(
+        uint256 requestId, 
+        uint256[] calldata randomWords
+        ) internal virtual override{}
+
+    /**Getter Functions **/ 
+    function getEntranceFee() external view returns(uint256){
+        return i_entranceFee;
+    }
+
+}
